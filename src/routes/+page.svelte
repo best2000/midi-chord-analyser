293e582f4385e12f8ad7d737504a5c19760ebff8
best2000 <silentman@hotmail.co.th>
2024-07-16 23:52:57 +0700

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,64 @@
 <script>
-    import { onMount } from 'svelte';
-    import '../styles/styles.css';
-    import {WebMidi} from "webmidi";
+    import { onMount } from "svelte";
+    import "../styles/styles.css";
+    import { WebMidi } from "webmidi";
 
-    let log = ""
+    let log = "";
+    $: console.log("log: " + log);
+    let midiInputs = [];
+    let selectedMidiInput = "";
+    $: console.log("selectedMidiInput: " + selectedMidiInput);
+
+    function listenMidiInput() {
+        const midiInput = WebMidi.getInputByName(selectedMidiInput);
+        midiInput.addListener("noteon", (e) => {
+            console.log(e.note.identifier);
+            log += e.note.identifier+"\n"
+        });
+    }
     
-    // onMount(()=>{
-        // alert("test")
-    // })
+    function clear() {
+        log = "";
+    }
+    function updateMidiInputs() {
+        midiInputs = WebMidi.inputs;
+        // console.log("midi inputs: " + WebMidi.inputs.length);
+        // WebMidi.inputs.forEach((input) => {
+        //     console.log(input.manufacturer, input.name);
+        // });
+    }
 
-    // Enable WEBMIDI.js and trigger the onEnabled() function when ready
-    WebMidi
-        .enable()
+    WebMidi.enable()
         .then(onEnabled)
-        .catch(err => console.log(err));
+        .catch((err) => (log += err));
 
     // Function triggered when WEBMIDI.js is ready
     function onEnabled() {
-
-        // Display available MIDI input devices
-        if (WebMidi.inputs.length < 1) {
-        console.log("No device detected.");
-        log+="No device detected"
-        } else {
-        WebMidi.inputs.forEach((device, index) => {
-            console.log(`WebMidi.inputs[${index}]: ${device.name}`);
-            log+=`WebMidi.inputs[${index}]: ${device.name}`
-        });
+        // Inputs
+        console.log("midi inputs: " + WebMidi.inputs.length);
+        midiInputs = WebMidi.inputs;
+        if (midiInputs.length != 0) {
+            selectedMidiInput = midiInputs[0].name
+            listenMidiInput()
         }
-
     }
-
-    function clear() {
-        log=""
-    }
-
 </script>
 
-<div class="home_hero">
+<div>
     <h1>MIDI Chord Analyser</h1>
 </div>
-
-<button on:click={clear}>clear</button>
+<label for="midi_input">MIDI Input:</label>
+<select
+    name="midi_input"
+    bind:value={selectedMidiInput}
+    on:focus={updateMidiInputs}
+    on:change={listenMidiInput}
+>
+    {#each midiInputs as input}
+        <option>{input.name}</option>
+    {/each}
+</select>
+<button on:focus={clear}>clear</button>
 <textarea style="width: 100%; height:200px; font-size:xx-small">
 {log}
 </textarea>
@@ -53,22 +70,21 @@
      4. The narrow part of white keys F, G, A, and B is W - B*3/4 -->
 <svg xml:space="preserve" width="100%" height="120">
     <!--  White keys   -->
-    <rect class="white-key" x="0" y="0" width="20" height="120"/>
-    <rect class="white-key" x="20" y="0" width="20" height="120"/>
-    <rect class="white-key" x="40" y="0" width="20" height="120"/>
-    <rect class="white-key" x="60" y="0" width="20" height="120"/>
-    <rect class="white-key" x="80" y="0" width="20" height="120"/>
-    <rect class="white-key" x="100" y="0" width="20" height="120"/>
-    <rect class="white-key" x="120" y="0" width="20" height="120"/>
-    <rect class="white-key" x="140" y="0" width="20" height="120"/>
-    <rect class="white-key" x="160" y="0" width="20" height="120"/>
-    <rect class="white-key" x="180" y="0" width="20" height="120"/>
-    <rect class="white-key" x="200" y="0" width="20" height="120"/>
+    <rect class="white-key" x="0" y="0" width="20" height="120" />
+    <rect class="white-key" x="20" y="0" width="20" height="120" />
+    <rect class="white-key" x="40" y="0" width="20" height="120" />
+    <rect class="white-key" x="60" y="0" width="20" height="120" />
+    <rect class="white-key" x="80" y="0" width="20" height="120" />
+    <rect class="white-key" x="100" y="0" width="20" height="120" />
+    <rect class="white-key" x="120" y="0" width="20" height="120" />
+    <rect class="white-key" x="140" y="0" width="20" height="120" />
+    <rect class="white-key" x="160" y="0" width="20" height="120" />
+    <rect class="white-key" x="180" y="0" width="20" height="120" />
+    <rect class="white-key" x="200" y="0" width="20" height="120" />
     <!--  Black keys (overlap with the white keys)  -->
-    <rect style="black-key" x="12.5" y="0" width="15" height="80"/>
-    <rect style="black-key" x="41.66666" y="0" width="13" height="80"/>
-    <rect style="black-key" x="82.25" y="0" width="13" height="80"/>
-    <rect style="black-key" x="108.25" y="0" width="13" height="80"/>
-    <rect style="black-key" x="134.75" y="0" width="13" height="80"/>
-    
+    <rect style="black-key" x="12.5" y="0" width="15" height="80" />
+    <rect style="black-key" x="41.66666" y="0" width="13" height="80" />
+    <rect style="black-key" x="82.25" y="0" width="13" height="80" />
+    <rect style="black-key" x="108.25" y="0" width="13" height="80" />
+    <rect style="black-key" x="134.75" y="0" width="13" height="80" />
 </svg>
