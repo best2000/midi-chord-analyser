91dd4a2b191ea307b1d17e6414148475f26116fb
best2000 <silentman@hotmail.co.th>
2024-07-17 23:28:34 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,21 @@
 <script>
     import "../styles.css";
     import { WebMidi } from "webmidi";
-    import { removeSameElementFromSet } from "../lib/index"
+    import { removeSameElementFromSet } from "../lib/index";
     import { Midi, Interval, Chord } from "tonal";
 
     let log_midi, log_var;
-    log_midi = log_var = ""
+    log_midi = log_var = "";
     let midiInputs = [];
     let selectedMidiInput = "";
-    $: if (selectedMidiInput) console.log("selectedMidiInput: "+selectedMidiInput)
+    $: if (selectedMidiInput)
+        console.log("selectedMidiInput: " + selectedMidiInput);
     let noteOn = new Set([]);
     let noteSustain = new Set([]);
     let pedal = false;
-    $: if (pedal) console.log("pedal: "+pedal)
-    let chordNotes = []
-    let chord = "?"
+    $: if (pedal) console.log("pedal: " + pedal);
+    let chordNotes = [];
+    let chord = "?";
 
     WebMidi.enable()
         .then(onEnabled)
@@ -29,18 +30,21 @@
     }
 
     function calulateChord() {
-        let notes = Array.from(new Set([...noteOn,...noteSustain]))
-        notes.sort()
-        chordNotes = notes
+        let notes = Array.from(new Set([...noteOn, ...noteSustain]));
+        notes.sort();
+        chordNotes = notes;
         if (notes.length == 1) {
-            chord = `${Midi.midiToNoteName(notes[0], { pitchClass: true })}` 
+            chord = `${Midi.midiToNoteName(notes[0], { pitchClass: true })}`;
         } else if (notes.length == 2) {
-            chord = `${Midi.midiToNoteName(notes[0], { pitchClass: true })} ${Interval.fromSemitones(notes[1]-notes[0])}`;
+            chord = `${Midi.midiToNoteName(notes[0], { pitchClass: true })} ${Interval.fromSemitones(notes[1] - notes[0])}`;
         } else {
-            let chordArr = Chord.detect(notes.map((n)=> Midi.midiToNoteName(n)), { assumePerfectFifth: true })
-            chord = chordArr.join("|")
+            let chordArr = Chord.detect(
+                notes.map((n) => Midi.midiToNoteName(n)),
+                { assumePerfectFifth: true },
+            );
+            chord = chordArr.join("|");
             if (chordArr.length == 0) {
-                chord = "?"
+                chord = "?";
             }
         }
     }
@@ -49,16 +53,18 @@
         //listen to all midi channels
         const midiInput = WebMidi.getInputByName(selectedMidiInput);
         midiInput.addListener("noteon", (e) => {
-            noteOn.add(e.note.number)
-            calulateChord()
-            
-            log_midi+= `note on  : ${e.note.name}${e.note.octave} : ${e.message.statusByte.toString(2)} : ${e.note.number}\n`;
-            log_var+=`on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`
-                
-            document.getElementById(e.note.number).style["fill"] = "red"
+            noteOn.add(e.note.number);
+            calulateChord();
+
+            log_midi += `note on  : ${e.note.name}${e.note.octave} : ${e.message.statusByte.toString(2)} : ${e.note.number}\n`;
+            log_var += `on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`;
+
+            document.getElementById(e.note.number).style["background-color"] = "red";
 
             let logMidiTextArea = document.getElementById("log_midi");
-            if (logMidiTextArea.selectionStart == logMidiTextArea.selectionEnd) {
+            if (
+                logMidiTextArea.selectionStart == logMidiTextArea.selectionEnd
+            ) {
                 logMidiTextArea.scrollTop = logMidiTextArea.scrollHeight;
             }
             let logVarTextArea = document.getElementById("log_var");
@@ -67,23 +73,25 @@
             }
         });
         midiInput.addListener("noteoff", (e) => {
-            noteOn.delete(e.note.number) 
+            noteOn.delete(e.note.number);
             if (pedal) {
-                noteSustain.add(e.note.number)
+                noteSustain.add(e.note.number);
             }
-            calulateChord()
+            else {
+                let key = document.getElementById(e.note.number);
+                if (key.className == "piano-keys white-key")
+                    key.style["background-color"] = "white";
+                else key.style["background-color"] = "black";
+            }
+            calulateChord();
 
             log_midi += `note off : ${e.note.name}${e.note.octave} : ${e.message.statusByte.toString(2)} : ${e.note.number}\n`;
-            log_var+=`on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`
-                
-            let key = document.getElementById(e.note.number)
-            if (key.className.baseVal == "white-key") 
-                key.style["fill"] = "white"
-            else 
-                key.style["fill"] = "black"
-            
+            log_var += `on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`;
+
             let logMidiTextArea = document.getElementById("log_midi");
-            if (logMidiTextArea.selectionStart == logMidiTextArea.selectionEnd) {
+            if (
+                logMidiTextArea.selectionStart == logMidiTextArea.selectionEnd
+            ) {
                 logMidiTextArea.scrollTop = logMidiTextArea.scrollHeight;
             }
             let logVarTextArea = document.getElementById("log_var");
@@ -94,31 +102,36 @@
         midiInput.addListener("controlchange", (e) => {
             if (e.subtype == "damperpedal") {
                 if (e.value) {
-                    pedal = true
+                    pedal = true;
                     log_midi += `pedal on\n`;
                 } else {
-                    pedal = false
-                    removeSameElementFromSet(noteOn,noteSustain)
-                    noteSustain.clear()
+                    pedal = false;
+                    removeSameElementFromSet(noteOn, noteSustain);
+                    noteSustain.clear();
                     log_midi += `pedal off\n`;
                 }
-                calulateChord()
-                log_var+=`on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`
-                
+                calulateChord();
+                log_var += `on[${Array.from(noteOn)}] : sustain[${Array.from(noteSustain)}] : chord[${chordNotes}]\n`;
+
                 let logMidiTextArea = document.getElementById("log_midi");
-                if (logMidiTextArea.selectionStart == logMidiTextArea.selectionEnd) {
+                if (
+                    logMidiTextArea.selectionStart ==
+                    logMidiTextArea.selectionEnd
+                ) {
                     logMidiTextArea.scrollTop = logMidiTextArea.scrollHeight;
                 }
                 let logVarTextArea = document.getElementById("log_var");
-                if (logVarTextArea.selectionStart == logVarTextArea.selectionEnd) {
+                if (
+                    logVarTextArea.selectionStart == logVarTextArea.selectionEnd
+                ) {
                     logVarTextArea.scrollTop = logVarTextArea.scrollHeight;
                 }
             }
         });
     }
     function clear() {
-        log_midi = ""
-        log_var = ""
+        log_midi = "";
+        log_var = "";
     }
 
     function updateMidiInputs() {
@@ -127,7 +140,6 @@
             console.log(input.manufacturer, input.name);
         });
     }
-    
 </script>
 
 <div>
@@ -146,35 +158,104 @@
     {/each}
 </select>
 <button on:focus={clear}>clear</button>
-<br/>
+<br />
 <textarea id="log_midi" style="width: 20%; height:200px; font-size:xx-small">
 {log_midi}
 </textarea>
 <textarea id="log_var" style="width: 75%; height:200px; font-size:xx-small">
 {log_var}
 </textarea>
-<br/>
+<br />
 <p>pedal: {pedal}</p>
-<!-- This keyboard has following properties (x=octave width).
-     1. All white keys have equal width in front (W=x/7).
-     2. All black keys have equal width (B=x/12).
-     3. The narrow part of white keys C, D and E is W - B*2/3
-     4. The narrow part of white keys F, G, A, and B is W - B*3/4 -->
-     <svg xml:space="preserve" width="100%" height="125">
-        <!--  White keys   -->
-        <rect id=60 class="white-key"  x="0" y="0" width="23" height="120"/>
-        <rect id=62 class="white-key"  x="23" y="0" width="23" height="120"/>
-        <rect id=64 class="white-key"  x="46" y="0" width="23" height="120"/>
-        <rect id=65 class="white-key"  x="69" y="0" width="23" height="120"/>
-        <rect id=67 class="white-key"  x="92" y="0" width="23" height="120"/>
-        <rect id=69 class="white-key"  x="115" y="0" width="23" height="120"/>
-        <rect id=71 class="white-key"  x="138" y="0" width="23" height="120"/>
-        <!--  Black keys (overlap with the white keys)  -->
-        <rect id=61 class="black-key"  x="14.33333" y="0" width="13" height="80"/>
-        <rect id=63 class="black-key"  x="41.66666" y="0" width="13" height="80"/>
-        <rect id=66 class="black-key"  x="82.25" y="0" width="13" height="80"/>
-        <rect id=68 class="black-key"  x="108.25" y="0" width="13" height="80"/>
-        <rect id=70 class="black-key"  x="134.75" y="0" width="13" height="80"/>
-    </svg>
-
-
+<div class="piano-container">
+    <ul class="piano-keys-list">
+        <li class="piano-keys white-key" id=21></li>
+        <li class="piano-keys black-key" id=22></li>
+        <li class="piano-keys white-key" id=23></li>
+        <li class="piano-keys white-key" id=24></li>
+        <li class="piano-keys black-key" id=25></li>
+        <li class="piano-keys white-key" id=26></li>
+        <li class="piano-keys black-key" id=27></li>
+        <li class="piano-keys white-key" id=28></li>
+        <li class="piano-keys white-key" id=29></li>
+        <li class="piano-keys black-key" id=30></li>
+        <li class="piano-keys white-key" id=31></li>
+        <li class="piano-keys black-key" id=32></li>
+        <li class="piano-keys white-key" id=33></li>
+        <li class="piano-keys black-key" id=34></li>
+        <li class="piano-keys white-key" id=35></li>
+        <li class="piano-keys white-key" id=36></li>
+        <li class="piano-keys black-key" id=37></li>
+        <li class="piano-keys white-key" id=38></li>
+        <li class="piano-keys black-key" id=39></li>
+        <li class="piano-keys white-key" id=40></li>
+        <li class="piano-keys white-key" id=41></li>
+        <li class="piano-keys black-key" id=42></li>
+        <li class="piano-keys white-key" id=43></li>
+        <li class="piano-keys black-key" id=44></li>
+        <li class="piano-keys white-key" id=45></li>
+        <li class="piano-keys black-key" id=46></li>
+        <li class="piano-keys white-key" id=47></li>
+        <li class="piano-keys white-key" id=48></li>
+        <li class="piano-keys black-key" id=49></li>
+        <li class="piano-keys white-key" id=50></li>
+        <li class="piano-keys black-key" id=51></li>
+        <li class="piano-keys white-key" id=52></li>
+        <li class="piano-keys white-key" id=53></li>
+        <li class="piano-keys black-key" id=54></li>
+        <li class="piano-keys white-key" id=55></li>
+        <li class="piano-keys black-key" id=56></li>
+        <li class="piano-keys white-key" id=57></li>
+        <li class="piano-keys black-key" id=58></li>
+        <li class="piano-keys white-key" id=59></li>
+        <li class="piano-keys white-key" id=60></li>
+        <li class="piano-keys black-key" id=61></li>
+        <li class="piano-keys white-key" id=62></li>
+        <li class="piano-keys black-key" id=63></li>
+        <li class="piano-keys white-key" id=64></li>
+        <li class="piano-keys white-key" id=65></li>
+        <li class="piano-keys black-key" id=66></li>
+        <li class="piano-keys white-key" id=67></li>
+        <li class="piano-keys black-key" id=68></li>
+        <li class="piano-keys white-key" id=69></li>
+        <li class="piano-keys black-key" id=70></li>
+        <li class="piano-keys white-key" id=71></li>
+        <li class="piano-keys white-key" id=72></li>
+        <li class="piano-keys black-key" id=73></li>
+        <li class="piano-keys white-key" id=74></li>
+        <li class="piano-keys black-key" id=75></li>
+        <li class="piano-keys white-key" id=76></li>
+        <li class="piano-keys white-key" id=77></li>
+        <li class="piano-keys black-key" id=78></li>
+        <li class="piano-keys white-key" id=79></li>
+        <li class="piano-keys black-key" id=80></li>
+        <li class="piano-keys white-key" id=81></li>
+        <li class="piano-keys black-key" id=82></li>
+        <li class="piano-keys white-key" id=83></li>
+        <li class="piano-keys white-key" id=84></li>
+        <li class="piano-keys black-key" id=85></li>
+        <li class="piano-keys white-key" id=86></li>
+        <li class="piano-keys black-key" id=87></li>
+        <li class="piano-keys white-key" id=88></li>
+        <li class="piano-keys white-key" id=89></li>
+        <li class="piano-keys black-key" id=90></li>
+        <li class="piano-keys white-key" id=91></li>
+        <li class="piano-keys black-key" id=92></li>
+        <li class="piano-keys white-key" id=93></li>
+        <li class="piano-keys black-key" id=94></li>
+        <li class="piano-keys white-key" id=95></li>
+        <li class="piano-keys white-key" id=96></li>
+        <li class="piano-keys black-key" id=97></li>
+        <li class="piano-keys white-key" id=98></li>
+        <li class="piano-keys black-key" id=99></li>
+        <li class="piano-keys white-key" id=100></li>
+        <li class="piano-keys white-key" id=101></li>
+        <li class="piano-keys black-key" id=102></li>
+        <li class="piano-keys white-key" id=103></li>
+        <li class="piano-keys black-key" id=104></li>
+        <li class="piano-keys white-key" id=105></li>
+        <li class="piano-keys black-key" id=106></li>
+        <li class="piano-keys white-key" id=107></li>
+        <li class="piano-keys white-key" id=108></li>
+    </ul>
+</div>
